53f342d2c3a436f18ff7c066d95158bf26b8aebe
Ian Singh <iansingh@ians-mbp-2.mynetworksettings.com>
2026-02-14 22:28:52 -0500

    Revert layout background

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body style={{ margin: 0, padding: 0, background: "#000000" }}>
+      <body style={{ margin: 0, padding: 0, background: "#0a0a0a" }}>
         {children}
       </body>
     </html>
